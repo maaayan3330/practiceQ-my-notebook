b3601356b6e442dc93c5f67f4bdc452bf28beec2
maayan3330 <maayan3330@gmail.com>
2025-04-21 18:32:03 +0300

    commit

---
+++ M+-chat-idea/longestRpeatingPrefix.py
@@ -0,0 +1,4 @@
+# Write a function that takes a string s and returns the length of the longest prefix that 
+# also appears again somewhere else in the string (not as the prefix itself).
+# The prefix cannot be the entire string.
+
